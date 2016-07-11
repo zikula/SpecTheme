fa5e51e85aafcab0e3e649842b1bc80c492922e0
Craig Heydenburg <craigh@mac.com>
2016-07-11 18:32:33 -0400

    remove headers

--- Twig/SpecThemeExtension.php
+++ Twig/SpecThemeExtension.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * Copyright Zikula Foundation 2015 - Zikula Application Framework
- *
- * This work is contributed to the Zikula Foundation under one or more
- * Contributor Agreements and licensed to You under the following license:
- *
- * @license MIT
- * @package SpecTheme
- *
- * Please see the NOTICE file distributed with this source code for further
- * information regarding copyright and licensing.
- */
 
 namespace Zikula\SpecTheme\Twig;
 
